5f575ac21faeb69bf304fc7b4987a9d571fd5a55
$CATO <84271741+SOL-CAT@users.noreply.github.com>
2021-11-09 14:33:37 +0530

    Listing

--- src/utils/markets.tsx
+++ src/utils/markets.tsx
@@ -870,64 +870,64 @@ const BMBOUSDCMarketsInfo = {
   address: new PublicKey("8dpaLWWPv6vFong1D8gHFDmYzHQreXuKcui3XCKBACCj"),
   deprecated: false,
   name : "BMBO/USDC",
-  qouteLable: "USDC",
-  baseLable: "BMBO",
+  quoteLabel: "USDC",
+  baseLabel: "BMBO",
   programId: new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 };
 const FLOOFUSDCMarketsInfo = {
   address: new PublicKey("BxcuT1p8FK9cFak4Uuf5nmoAZ7nQGu7FerCMESGqxF7b"),
   deprecated: false,
   name : "FLOOF/USDC",
-  qouteLable: "USDC",
-  baseLable: "FLOOF",
+  quoteLabel: "USDC",
+  baseLabel: "FLOOF",
   programId: new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 };
 const SCUMUSDCMarketsInfo = {
   address: new PublicKey("ArKuemtzjpqP5H3P4S21nzhtLZtsq7nMCN6ecA9yvMX9"),
   deprecated: false,
   name : "SCUM/USDC",
-  qouteLable: "USDC",
-  baseLable: "SCUM",
+  quoteLabel: "USDC",
+  baseLabel: "SCUM",
   programId: new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 };
 const SHIBAUSDCMarketsInfo = {
   address: new PublicKey("3M8uZhLZMxFUedsEgPzywZr9qbGTv3kKNMCEfAmg8iyK"),
   deprecated: false,
   name : "SHIBA/USDC",
-  qouteLable: "USDC",
-  baseLable: "SHIBA",
+  quoteLabel: "USDC",
+  baseLabel: "SHIBA",
   programId: new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 };
 const SCHADUSDCMarketsInfo = {
   address: new PublicKey("7oTiNcYoQiS4u4fYU6y7VDE8eCKWcdxa1qccwJgVwn8q"),
   deprecated: false,
   name : "SCHAD/USDC",
-  qouteLable: "USDC",
-  baseLable: "SCHAD",
+  quoteLabel: "USDC",
+  baseLabel: "SCHAD",
   programId: new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 };
 const INUUSDCMarketsInfo = {
   address: new PublicKey("G3Bss3a2tif6eHNzWCh14g5k2H4rwBAmE42tbckUWG5T"),
   deprecated: false,
   name : "INU/USDC",
-  qouteLable: "USDC",
-  baseLable: "INU",
+  quoteLabel: "USDC",
+  baseLabel: "INU",
   programId: new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 };
 const NRAUSDCMarketsInfo = {
   address: new PublicKey("5JC2kckdehvq8k2JMZE9gJzdiyhGyg5CH4HN8gxwWSyo"),
   deprecated: false,
   name : "NRA/USDC",
-  qouteLable: "USDC",
-  baseLable: "NRA",
+  quoteLabel: "USDC",
+  baseLabel: "NRA",
   programId: new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 };
 const HIMAUSDCMarketsInfo = {
   address: new PublicKey("HCE4wQXApNyFBTK7gYa98QCYbshCz7EkH8axNz3ahvKc"),
   deprecated: false,
   name : "HIMA/USDC",
-  qouteLable: "USDC",
-  baseLable: "HIMA",
+  quoteLabel: "USDC",
+  baseLabel: "HIMA",
   programId: new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
 };
 
